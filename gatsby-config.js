9d376498b4f379112f034e45bcd903927d9cb504
Fernando Cutire UTP <fernando.cutire@utp.ac.pa>
2020-01-09 20:37:07 +0000

    El diseño inicial listo

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,7 +3,7 @@ const config = require('./config');
 module.exports = {
   pathPrefix: config.pathPrefix,
   siteMetadata: {
-    title: config.siteTitle,
+    title: "Fernando Cutire",
   },
   plugins: [
     'gatsby-plugin-react-helmet',
